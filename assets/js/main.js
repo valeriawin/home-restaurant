124119d727cf004ce7e37a49058ab6e9630ce5a0
e.petrova <e.petrova@etence.org>
2022-11-05 20:46:29 +0300

    add home mobile section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,36 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close');
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu')
+    })
+}
 
-
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu')
+    })
+}
 /*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link');
+
+const linkAction = () => {
+    navMenu.classList.remove('show-menu')
+}
+
+navLink.forEach(e => e.addEventListener('click', linkAction));
 
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+
+const scrollHeader = () => {
+    const header = document.getElementById('header');
+    this.scrollY >= 50 ? header.classList.add('bg-header') : header.classList.remove('bg-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 
 /*=============== SHOW SCROLL UP ===============*/ 
